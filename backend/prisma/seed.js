9ef42618012d2a69479232eeb892feaeeb042208
Deepak Chauhan <deepak41125@gmail.com>
2026-04-21 10:01:49 +0530

    Docker file added

---
+++ backend/prisma/seed.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
+async function main() {
+    await prisma.role.createMany({
+        data: [
+            { name: 'ADMIN' },
+            { name: 'EMPLOYEE' },
+        ],
+        skipDuplicates: true,
+    });
+    console.log('✅ Roles seeded successfully');
+}
+main()
+    .catch((e) => {
+    console.error(e);
+})
+    .finally(async () => {
+    await prisma.$disconnect();
+});
+//# sourceMappingURL=seed.js.map
