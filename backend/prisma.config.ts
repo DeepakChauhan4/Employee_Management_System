e1faf7c9373d8f69c173150a070f4bb619b086f9
Deepak Chauhan <deepak41125@gmail.com>
2026-04-22 12:37:43 +0530

    updated prisma.config.ts file

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,10 +1,5 @@
-import { defineConfig, env } from '@prisma/config'; // Make sure env is imported
-import 'dotenv/config';
+const { defineConfig } = require("prisma/config");
 
-export default defineConfig({
-  schema: 'prisma/schema.prisma',
-  datasource: {
-    // Use Prisma's env() function instead of process.env
-    url: env('DATABASE_URL'),
-  },
+module.exports = defineConfig({
+  schema: "prisma/schema.prisma",
 });
